b44128b59c820b81c76bd42947727d4d140910f2
HimanshuYadav1811 <himanshuyadav181995@gmail.com>
2026-03-13 18:59:27 +0530

    update

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <LayoutTopHeader />
+    <!-- <LayoutTopHeader /> -->
     <LayoutMainNavbar />
     <PrivacyPolicy />
     <LayoutMainFooter />
--- pages/terms-conditions.vue
+++ pages/terms-conditions.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <LayoutTopHeader />
+    <!-- <LayoutTopHeader /> -->
     <LayoutMainNavbar />
     <TermsConditions />
     <LayoutMainFooter />
