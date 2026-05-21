429e76604a736509977edb43e436743958799b0f
suneel <suneel.kumar@kcglobed.com>
2026-05-21 11:40:04 +0530

    add google tag manager, meta pixel code, microsoft clarity code

--- app.vue
+++ app.vue
@@ -1,6 +1,4 @@
 <template>
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5NVDGXLR" height="0" width="0"
-      style="display:none;visibility:hidden"></iframe></noscript>
   <NuxtLayout></NuxtLayout>
 </template>
 
@@ -22,6 +20,35 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5NVDGXLR');`
+    },
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-B2ETHYM6MN',
+      async: true
+    },
+    {
+      innerHTML: `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-B2ETHYM6MN');`
+    },
+    {
+      innerHTML: `!function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1308129784468579');
+      fbq('track', 'PageView');`
+    },
+    {
+      innerHTML: `(function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "wtdnppatos");`
     },
     {
       type: 'application/ld+json',
@@ -83,6 +110,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           "closes": "06:30"
         }
       })
+    }
+  ],
+  noscript: [
+    {
+      innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5NVDGXLR" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+      tagPosition: 'bodyOpen'
+    },
+    {
+      innerHTML: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1308129784468579&ev=PageView&noscript=1" />`,
+      tagPosition: 'bodyOpen'
     }
   ]
 });
