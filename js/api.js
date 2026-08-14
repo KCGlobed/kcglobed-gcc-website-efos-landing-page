1161c0bc0836caa9c89f0e3cba6c28570ab60382
rupesh <102rupeshsharma@gmail.com>
2026-08-14 13:08:00 +0530

    update

--- js/api.js
+++ js/api.js
@@ -14,6 +14,7 @@ if (window.location.hostname.includes("gccschool.com")) {
     mode = "sandbox";
 }
 
+
 export const CONFIG = {
     BACKEND_API_BASE: GCC_BACKEND_URL,
     LOCAL_API_BASE: BASE_URL,
